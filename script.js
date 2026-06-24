35972facadb84e4847618b4d3fac8dbd00150690
Hugol4ka <hugolaca12300@gmail.com>
2026-06-24 12:20:56 +0200

    Added calculation for the "release date" counter

---
+++ script.js
@@ -0,0 +1,22 @@
+/* COMPTEUR */
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+//Date de sortie du jeu.
+const countDownDate = new Date("November 19, 2026 00:00:00").getTime();
+
+/* Met à jour le compte à rebours toutes les secondes */
+const x = setInterval(function() {
+const now = new Date().getTime();
+const distance = countDownDate - now;
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+daysElement.textContent = days;
+hoursElement.textContent = hours;
+minutesElement.textContent = minutes;
+secondsElement.textContent = seconds;
+}, 1000);
